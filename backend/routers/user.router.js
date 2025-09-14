526ab7e9b1c735ba137a776392610c4594fe3d94
sheetal <er.sheetalbhardwaj@gmail.com>
2025-09-14 17:23:41 +0530

    backend update

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -3,7 +3,6 @@ import protectRoute from "../middleware/protectRoute.js";
 import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
-
 router.get("/", protectRoute, getUsersForSidebar);
 
 export default router;
